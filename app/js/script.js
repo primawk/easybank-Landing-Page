63082daa3b515d57c90e9b6e2115e6db76eca7d0
primawk <pwkuntoro@gmail.com>
2022-03-14 22:59:46 +0700

    animated mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,27 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body")
 const header = document.querySelector(".header"); // impact to class header
 const overlay = document.querySelector(".overlay"); // impact to class overlay
+const fadeElems = document.querySelectorAll(".has-fade")
+
 
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
   if (header.classList.contains("open")) {
     // close hamburger menu
+    body.classList.remove("noscroll")
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElems.forEach(function(element){
+      element.classList.remove("fade-in"); // adds fade-out in overlay class
+      element.classList.add("fade-out"); // adds fade-in class in overlay class
+    })
   } else {
     // open hamburger menu
+    body.classList.add("noscroll")
     header.classList.add("open"); // adds open in header class
-    overlay.classList.remove("fade-out"); // adds fade-out in overlay class
-    overlay.classList.add("fade-in"); // adds fade-in class in overlay class
+    fadeElems.forEach(function(element){
+      element.classList.remove("fade-out"); // adds fade-out in overlay class
+      element.classList.add("fade-in"); // adds fade-in class in overlay class
+    })
   }
 });
